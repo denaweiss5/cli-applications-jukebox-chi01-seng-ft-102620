163ac3741dc2225244535a318b108890e0bd3299
Dena Neuman <denaweiss5@gmail.com>
2020-10-23 02:39:15 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -49,27 +49,19 @@ end
 
 def run(songs)
   help
-   puts "Please enter a command:"
-   user_response = gets.chomp
-   while user_response != "exit"
-     case user_response
-       when "help"
-         help
-         puts "Please enter a command:"
-         user_response = gets.chomp
-       when "list"
-         list(songs)
-         puts "Please enter a command:"
-         user_response = gets.chomp
-       when "play"
-         play(songs)
-         puts "Please enter a command:"
-         user_response = gets.chomp
-       when "exit"
-         exit_jukebox
-       else
-         puts "Invalid input, please try again:"
-         user_response = gets.chomp
-       end
-     end
- end
+  loop do
+    puts "Please enter a command:"
+    input = gets.chomp
+    case input
+   when 'play'
+     play(songs)
+   when 'help'
+     help
+   when 'list'
+     list(songs)
+   when 'exit'
+     break
+   end
+   end
+     exit_jukebox
+   end
